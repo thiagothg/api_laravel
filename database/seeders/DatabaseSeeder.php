b59ee4fc696b8bae56532aeaae06a6ed1228321b
Thiago Rodrigues <thiago_thg@outlook.com>
2022-07-14 21:45:07 -0300

    Migration Series, sessons

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +19,11 @@ class DatabaseSeeder extends Seeder
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        //     'email' => 'admin@example.com',
+        //     'password' => Hash::make('123')
         // ]);
+
+        \App\Models\Series::factory(10)->create();
+
     }
 }
